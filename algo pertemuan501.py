ab19e5f526a368c131719db93e5e760bdd68b93e
ayu2401010171 <diahpurnamadewi711@gmail.com>
2024-12-05 12:20:53 +0800

    Add files via upload

---
+++ algo pertemuan501.py
@@ -0,0 +1,5 @@
+kalimat = 'stiki'
+print('Kalimat :',kalimat)
+print('\'i\' in kalimat     :', 'i' in kalimat)
+print('\'z\' not in kalimat :', 'z' not in kalimat)
+print('\'d\' not in kalimat :', 'd' not in kalimat)
---
+++ algo pertemuan502.py
@@ -0,0 +1,4 @@
+name = 'belajar python'
+print ("name[0]: ", name[0])
+print ("name[0:3]: ", name[0:3])
+print ("name[1:4]: ", name[1:4])
